7baf5d491261820d8bc15907a1a0b612b56ace5d
pikipik26 <riverosy859@gmail.com>
2024-04-24 21:25:41 -0300

    practica funciones

--- funciones/practica_24/04.py
+++ funciones/practica_24/04.py
@@ -23,6 +23,8 @@ def suma(num1,num2):
 
 
 def suma_bucle():
+    resultado=[]
+    total=0
     while True : 
         num1=int(input(" ingrse el primer valor (ingrese un numero nrgativo para salir; )"))
         if num1 < 0:
@@ -30,8 +32,19 @@ def suma_bucle():
             break
         num2=int(input("ingrse el segundo valor: "))
         resultado_suma=suma(num1,num2)
+        resultado.append(resultado_suma)
         print(f"la suma de {num1} y {num2} es: {resultado_suma}")
-suma_bucle()        
+    print(f"el resultado de la suma es: {sum(resultado)} ")
+    for indice,resultado in enumerate(resultado,start=1):
+        print(f"suma{indice}: {resultado}")    
+suma_bucle()  
+
+# #TODO:  enumerate ==> es una funcion en python que e utiliza para enumerar elementosde una secuencia (lista,tuplas,etc),mientras los recorre en un bucle. enumerate toma dos parametros: la secuencia que se quiere sumar y opcionalmente el indice inicial,devuelve un objeto enumerado que produce tuplas que  contiene un contador y un elemento de la secuenco
+# frutas=["banana","pomelo","uva","mandarina"]
+# for indice,frutas in enumerate(frutas,start=1):
+#     print(f"{indice}: {frutas}: ")
+#lo que se solicita es que guarden todos los resultados de la suma y mostrarlos al salir del bicle
+
 
 
 
