f8bad40a08c9d644197dd3002bc4008cd07aa6e0
pikipiki26 <riverosy859@gmail.com>
2024-07-12 21:26:29 -0300

    sistema de productos

--- clase_polimorfismo/SISTEMA_DE_TRABAJADORES/proyecto_stock/productos/urls.py
+++ clase_polimorfismo/SISTEMA_DE_TRABAJADORES/proyecto_stock/productos/urls.py
@@ -4,4 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('',Listar_Producto, name='listado_productos'),
+    path('crear/',crear_producto, name='crear_producto'),
+    path('editar/<id>', Editar_producto, name='Editar_producto'),
+    path('actualizar/ <id>', Actualizar_producto, name= 'Actualizar_producto'),
 ]
--- clase_polimorfismo/SISTEMA_DE_TRABAJADORES/proyecto_stock/productos/views.py
+++ clase_polimorfismo/SISTEMA_DE_TRABAJADORES/proyecto_stock/productos/views.py
@@ -1,6 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import Producto
 # Create your views here.
 def Listar_Producto(request):#es una petision
     productos=Producto.objects.all()
     return render(request,"productos.html",{'productos':productos})
+
+def crear_producto(request):
+    nombre =request.POST["nombre"]
+    descripcion = request.POST["descripcion"]
+    precio = request.POST["precio"]
+    stock = request.POST["stock"]   
+    producto= Producto(nombre= nombre, descripcion=descripcion,precio=precio,stock=stock)
+    producto.save()
+    return redirect('/inventario')
+
+
+def Editar_producto(request,id):
+    producto= Producto.objects.get(id=id)
+    return render(request, 'Editar_producto.html',{'producto': producto})
+
+def Actualizar_producto(request,id):
+    producto=Producto.objects.get(id=id)
+    nombre =request.POST["nombre"]
+    descripcion = request.POST["descripcion"]
+    precio = request.POST["precio"]
+    stock = request.POST["stock"]   
+    producto.nombre= nombre
+    producto.descripcion= descripcion
+    producto.precio= precio
+    producto.stock= stock
+    producto.save()
+    return redirect('/inventario')
